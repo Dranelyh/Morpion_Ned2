08f22dc3f519ba326294e247e6cd36c8e0c5854a
Dranelyh <kylianbdu13@gmail.com>
2025-05-12 15:14:03 +0200

    voila

--- morpion.py
+++ morpion.py
@@ -1,5 +1,6 @@
 from pyniryo import *
 from minimax import *
+import copy
 
 def detect_objects_positions(robot, coordCases, game, depth = 0.32):
     img_compressed = robot.get_img_compressed()
@@ -9,7 +10,7 @@ def detect_objects_positions(robot, coordCases, game, depth = 0.32):
     img_threshold = morphological_transformations(img_threshold, morpho_type=MorphoType.OPEN,
                                                   kernel_shape=(11, 11), kernel_type=KernelType.ELLIPSE)
     # Trouver les contours dans l'image seuillée
-    cnts = biggest_contours_finder(img_threshold, 10)  # Trouver les 5 plus gros contours
+    cnts = biggest_contours_finder(img_threshold, 10)  # Trouver les 10 plus gros contours
     
     x_origin = 195
     y_origin = 305
@@ -33,9 +34,9 @@ def detect_objects_positions(robot, coordCases, game, depth = 0.32):
             x_real = (cx - x_origin) * depth / fx
             y_real = (y_origin - cy) * depth / fy
             
-            closest_case = get_closest_case([x_real, y_real], coordCases)
+            i, j = get_closest_case([x_real, y_real], coordCases)
             shape = detect_shape(cnt)
-            game[closest_case] = shape
+            game[i][j] = shape
 
             # Pour visualisation, dessiner le contour et le barycentre
             img_debug = draw_contours(img_threshold, [cnt])
@@ -76,22 +77,18 @@ def move_to_object(robot, object_position, z_position=0.1, depth=0.32):
     robot.move_pose(pose, "Morpion")
     
 def get_closest_case(position, cases):
-    """
-    Trouve l'index de la case la plus proche de la position donnée.
-    :param position: tuple (x, y) - coordonnées de l'objet
-    :param cases: liste des positions des centres des cases
-    :return: index de la case la plus proche
-    """
     min_distance = float('inf')
-    closest_index = -1
-    for i, case in enumerate(cases):
-        dx = position[0] - case[0]
-        dy = position[1] - case[1]
-        distance = (dx**2 + dy**2)**0.5
-        if distance < min_distance:
-            min_distance = distance
-            closest_index = i
-    return closest_index
+    closest_coords = (0, 0)
+    for i in range(3):
+        for j in range(3):
+            case = cases[i][j]
+            dx = position[0] - case[0]
+            dy = position[1] - case[1]
+            distance = (dx**2 + dy**2)**0.5
+            if distance < min_distance:
+                min_distance = distance
+                closest_coords = (i, j)
+    return closest_coords
     
 def detect_shape(cnt):
     """
@@ -106,40 +103,38 @@ def detect_shape(cnt):
         return 1
         
 def check_winner(game):
-    lines = [
-        [0, 1, 2],  # ligne 1
-        [3, 4, 5],  # ligne 2
-        [6, 7, 8],  # ligne 3
-        [0, 3, 6],  # colonne 1
-        [1, 4, 7],  # colonne 2
-        [2, 5, 8],  # colonne 3
-        [0, 4, 8],  # diagonale 1
-        [2, 4, 6],  # diagonale 2
-    ]
-    
-    for line in lines:
-        if game[line[0]] == game[line[1]] == game[line[2]] and game[line[0]] != 0:
-            if game[line[0]] == 1:  # Retourne le gagnant (0 ou 1)
-                print("Le joueur '1' (cercle) a gagné.")
-            else:
-                print("Le joueur '0' (carré) a gagné.")
+    # Vérifie lignes et colonnes
+    for i in range(3):
+        if game[i][0] == game[i][1] == game[i][2] != 0:
+            print(f"Le joueur '{game[i][0]}' a gagné (ligne {i+1}).")
             return False
-                
-    # Vérification d'égalité (si toutes les cases sont remplies et qu'il n'y a pas de gagnant)
-    if all(x != 0 for x in game):
+        if game[0][i] == game[1][i] == game[2][i] != 0:
+            print(f"Le joueur '{game[0][i]}' a gagné (colonne {i+1}).")
+            return False
+    
+    # Vérifie les diagonales
+    if game[0][0] == game[1][1] == game[2][2] != 0:
+        print(f"Le joueur '{game[0][0]}' a gagné (diagonale principale).")
+        return False
+    if game[0][2] == game[1][1] == game[2][0] != 0:
+        print(f"Le joueur '{game[0][2]}' a gagné (diagonale secondaire).")
+        return False
+
+    # Vérifie égalité
+    if all(cell != 0 for row in game for cell in row):
         print("La partie est terminée par égalité.")
         return False
-    
-    # La partie continue si aucune condition n'est remplie
+
     print("La partie continue.")
     return True
 
 def play(case, pionPose, observationJoints, coup):
+    i, j = coup
     robot.move_pose(pionPose)
     robot.close_gripper(400)
 
     robot.move_joints(observationJoints)
-    robot.move_pose(PoseObject(case[coup[0]*3 + coup[1]][0], case[coup[0]*3 + coup[1]][1], 0.09, 0, 1.57, 0), "Morpion")
+    robot.move_pose(PoseObject(case[i][j][0], case[i][j][1], 0.09, 0, 1.57, 0), "Morpion")
     robot.open_gripper(400)
 
     robot.move_joints(observationJoints)
@@ -153,33 +148,17 @@ if __name__ == '__main__':
     robot.set_contrast(1.0)
     robot.set_saturation(1.1)
     
-    #if "Morpion" not in robot.get_workspace_list():
-    #    workspacePoints = [[0.3813, 0.0843, 0.0281],
-    #                    [0.3759, -0.0870, 0.0274],
-    #                    [0.2025, -0.0837, 0.0192],
-    #                    [0.2090, 0.0874, 0.0192]]
-    #    robot.save_workspace_from_points("Morpion", workspacePoints[0], workspacePoints[1], workspacePoints[2], workspacePoints[3])
-    
-    origin = [0.288, 0, 0.05]
-    x_axe = [0.288, -0.10, 0.05]
-    y_axe = [0.35, 0, 0.05]
-    #robot.save_dynamic_frame_from_points("Morpion", "Un repère dynamique prenant comme origine le centre du plateau de jeu", origin, x_axe, y_axe, True)
-    
     origin2 = [0.2029, 0.0868, 0.0188]
     x_axe2 = [0.2004, -0.0858, 0.0190]
     y_axe2 = [0.3718, 0.0836,0.0272]
     robot.save_dynamic_frame_from_points("Morpion", "Un repère dynamique prenant comme origine le repère en bas en gauche du workspace", origin2, x_axe2, y_axe2)
     
     #enregistre la position relative en x et y des cases dans le repère dynamique Morpion en partant de haut à gauche et en finissant en bas à droite
-    caseMorpion = [[0.025, 0.155],
-                    [0.09, 0.155],
-                    [0.155, 0.155],
-                    [0.025, 0.09],
-                    [0.09, 0.09],
-                    [0.155, 0.09],
-                    [0.025, 0.025],
-                    [0.09, 0.025],
-                    [0.155, 0.025]]
+    caseMorpion = [
+    [[0.025, 0.155], [0.09, 0.155], [0.155, 0.155]],
+    [[0.025, 0.09],  [0.09, 0.09],  [0.155, 0.09]],
+    [[0.025, 0.025], [0.09, 0.025], [0.155, 0.025]]
+    ]
     
     homeJoints = [0, 0.5, -1.25, 0, 0, 0]
     observationJoints = [0, 0.22, -0.4, 0, -1.6, 0] 
@@ -187,37 +166,13 @@ if __name__ == '__main__':
     pickPose = PoseObject(0.2768, -0.1265, 0.1091, 0.177, 1.251, -1.431)
     
     #Donne l'état actuel du jeu, -1 si pas de pion, 0 pour un carré et 1 pour un cercle
-    game = [0 for i in range(9)]
-    newGame = game.copy()
+    game = [[0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+            ]
+    newGame = copy.deepcopy(game)
     
     playing = True
-    
-    #robot.move_joints(homeJoints)
-    #robot.open_gripper(400)
-    
-    #robot.move_joints(observationJoints)
-    #robot.move_pose(pickPoses[0])
-    #robot.move_pose(pickPoses[1])
-    #robot.close_gripper(400)
-    #robot.move_pose(pickPoses[0])
-
-    #robot.move_joints(observationJoints)
-
-    #robot.move_pose(PoseObject(caseMorpion[0][0], caseMorpion[0][1], 0.09, 0, 1.57, 0), "Morpion")
-    #robot.open_gripper(400)
-    
-    #if object_positions:
-    #    for i in range(len(object_positions)):
-    #        move_to_object(robot, object_positions[i])
-    #        robot.wait(2)
-    
-    #robot.move_pose(PoseObject(0, 0, 0.2, 0, 1.57, 0), "Morpion")
-    #robot.move_pose(PoseObject(0, 0.1, 0, 0, 1.57, 0), "Morpion")
-    #robot.move_pose(PoseObject(0.1, 0, 0, 0, 1.57, 0), "Morpion")
-    
-    #robot.move_joints(homeJoints)
-    
-    #robot.delete_workspace("Morpion")
 
     while playing :
         change = False
@@ -225,12 +180,12 @@ if __name__ == '__main__':
             detect_objects_positions(robot, caseMorpion, newGame)
             if newGame != game:
                 change = True
-                game = newGame.copy()
+                game = copy.deepcopy(newGame)
             print("waiting")
             robot.wait(3)
         print("Etat du jeu :", newGame)
         playing = check_winner(game)
-        #trouve prochain coups --> à faire
+        print(meilleur_coup(game))
         #check_winner(game, playing)
     
     robot.close_connection()
